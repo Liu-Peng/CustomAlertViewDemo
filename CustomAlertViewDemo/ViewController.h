8a683cfc6fa6f81d4856f475effb792e354f990e
LiuPeng <LiuPeng@mallcoo.cn>
2016-05-23 11:08:42 +0800

    test

---
+++ CustomAlertViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomAlertViewDemo
+//
+//  Created by admin on 16/5/23.
+//  Copyright © 2016年 LiuPeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios , '7.0'
+source 'https://github.com/CocoaPods/Specs.git'
+target 'CustomAlertViewDemo' do
+	pod 'AFNetworking', '~> 3.0'
+	pod 'Masonry', '~> 0.6.4'
+end
